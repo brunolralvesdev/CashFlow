d1e2b399127650fbffb644522a289ba4718bcb5a
BAlves <brunolralvesdev@gmail.com>
2024-04-13 00:08:20 -0300

    UseCase para registrar gastos

---
+++ src/CashFlow.Application/UseCases/Expenses/Register/RegisterExpenseUseCase.cs
@@ -0,0 +1,37 @@
+﻿using CashFlow.Communication.Enum;
+using CashFlow.Communication.Requests;
+using CashFlow.Communication.Responses;
+
+namespace CashFlow.Application.UseCases.Expenses.Register
+{
+    public class RegisterExpenseUseCase
+    {
+        public ResponseRegisteredExpenseJson Execute(RequestRegisterExpenseJson request)
+        {
+            Validate(request);
+
+
+            return new ResponseRegisteredExpenseJson
+            {
+                Id = Guid.NewGuid(),
+                Title = request.Title
+            };
+        }
+
+        private void Validate(RequestRegisterExpenseJson request)
+        {
+            var titleIsEmpty = string.IsNullOrWhiteSpace(request.Title);
+
+            if (titleIsEmpty) 
+                throw new ArgumentException("The title is required.");
+
+            if (request.Amount <= 0)
+                throw new ArgumentException("The Amount must to be greater than zero.");
+
+            var paymentTypeIsValid = Enum.IsDefined(typeof(EPaymentType), request.PaymentType);
+
+            if (paymentTypeIsValid == false)
+                throw new ArgumentException("This payment type is invalid.");
+        }
+    }
+}
